f587ed474a6a44aad676f8edbd352f76144e8047
dimitrije <dimitrije.stojkovic@enetelsolutions.com>
2025-01-31 15:12:47 +0100

    Added controller, repository, domain and service

---
+++ product-service/src/main/java/com/shop/product_service/domain/Product.java
@@ -0,0 +1,28 @@
+package com.shop.product_service.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private int quantity;
+}
---
+++ product-service/src/main/java/com/shop/product_service/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.shop.product_service.repository;
+
+import com.shop.product_service.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
